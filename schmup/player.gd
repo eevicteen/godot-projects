75daca15c4f272ab9e17e2fb4800734d8044c749
eevicteen <brycerivero@gmail.com>
2025-09-25 11:15:14 +0800

    added naive collision detection

--- schmup/player.gd
+++ schmup/player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 const SPEED = 420
+var health = 5
+var test_counter = 1
 
 var screen_size
 
@@ -33,4 +35,13 @@ func _physics_process(delta: float) -> void:
 		#$AnimatedSprite2D.flip_h = velocity.x < 0
 	#elif velocity.y != 0:
 		#$AnimatedSprite2D.animation = "up"
-		#$AnimatedSprite2D.flip_v = velocity.y > 0
+		#$AnimatedSprite2D.flip_v = velocity.y > 0	
+	 
+func _on_area_entered(area: Area2D) -> void:
+	print('heeree')
+	if area.is_in_group("mob"):
+		print("Player takes damage from mob")
+		health -= 1
+	elif area.is_in_group("bullet"):
+		print("Player takes damage from bullet")
+		health -= 1 # Replace with function body.
